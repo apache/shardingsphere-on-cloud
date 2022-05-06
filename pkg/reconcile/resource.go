6782f9901b174e4f8d2d5c6b670181987f5b1a53
Ghoul_Lee <windghoul@gmail.com>
2022-05-06 16:49:59 +0800

    Update resources based on CRD objects (#7)
    * ADD: Update proxy CRD cascaded resource logic
    FIX: Fix some logs format
    UPDATE: Status logic and update some status logic function
    
    * UPDATE: 1. Update LICENSE 2. Update status function 3. Change updateDeployment,add some default value
    ADD: 1. Update or correct the service and deployment resource according to the CRD object
    
    * UPDATE: update license to author Beijing Sifei Software Technology Co

--- pkg/reconcile/resource.go
+++ pkg/reconcile/resource.go
@@ -1,17 +1,18 @@
 /*
- * Copyright (c) 2022.
+ *   Copyright © 2022，Beijing Sifei Software Technology Co., LTD.
+ *   All Rights Reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  */
 
 package reconcile
@@ -117,12 +118,9 @@ func ConstructCascadingService(proxy *shardingspherev1alpha1.Proxy) *v1.Service 
 			Type: proxy.Spec.ServiceType.Type,
 			Ports: []v1.ServicePort{
 				{
-					Name: "proxy-port",
-					TargetPort: intstr.IntOrString{
-						Type:   intstr.Int,
-						IntVal: proxy.Spec.Port,
-					},
-					Port: proxy.Spec.Port,
+					Name:       "proxy-port",
+					TargetPort: fromInt32(proxy.Spec.Port),
+					Port:       proxy.Spec.Port,
 				},
 			},
 		},
@@ -166,6 +164,35 @@ else echo failed;exit 1;fi;mv /mysql-connector-java-{{ .Version }}.jar /opt/shar
 			EmptyDir: &v1.EmptyDirVolumeSource{},
 		},
 	})
+
+}
+
+func updateInitContainer(dp *appsv1.Deployment, mysql *shardingspherev1alpha1.MySQLDriver) {
+
+	if len(dp.Spec.Template.Spec.InitContainers) != 0 {
+		scriptStr := strings.Builder{}
+		t1, _ := template.New("shell").Parse(`wget https://repo1.maven.org/maven2/mysql/mysql-connector-java/{{ .Version }}/mysql-connector-java-{{ .Version }}.jar;
+wget https://repo1.maven.org/maven2/mysql/mysql-connector-java/{{ .Version }}/mysql-connector-java-{{ .Version }}.jar.md5;
+if [ $(md5sum /mysql-connector-java-{{ .Version }}.jar | cut -d ' ' -f1) = $(cat /mysql-connector-java-{{ .Version }}.jar.md5) ];
+then echo success;
+else echo failed;exit 1;fi;mv /mysql-connector-java-{{ .Version }}.jar /opt/shardingsphere-proxy/ext-lib`)
+		_ = t1.Execute(&scriptStr, mysql)
+		dp.Spec.Template.Spec.InitContainers = []v1.Container{
+			{
+				Name:    "download-mysql-connect",
+				Image:   "busybox:1.35.0",
+				Command: []string{"/bin/sh", "-c", scriptStr.String()},
+				VolumeMounts: []v1.VolumeMount{
+					{
+						Name:      "mysql-connect-jar",
+						MountPath: "/opt/shardingsphere-proxy/ext-lib",
+					},
+				},
+			},
+		}
+	} else {
+		addInitContainer(dp, mysql)
+	}
 
 }
 
@@ -255,4 +282,79 @@ func toYaml(proxyConfig *shardingspherev1alpha1.ProxyConfig) string {
 	}
 	y, _ := yaml.Marshal(proxyConfig.Spec)
 	return string(y)
+}
+
+// UpdateDeployment FIXME:merge UpdateDeployment and ConstructCascadingDeployment
+func UpdateDeployment(proxy *shardingspherev1alpha1.Proxy, runtimeDeployment *appsv1.Deployment) {
+	runtimeDeployment.Spec.Template.Spec.Containers[0].Image = fmt.Sprintf("apache/shardingsphere-proxy:%s", proxy.Spec.Version)
+	runtimeDeployment.Spec.Replicas = &proxy.Spec.Replicas
+	runtimeDeployment.Spec.Template.Spec.Volumes[0].ConfigMap.Name = proxy.Spec.ProxyConfigName
+	runtimeDeployment.Spec.Template.Spec.Containers[0].Env[0].Value = strconv.FormatInt(int64(proxy.Spec.Port), 10)
+	runtimeDeployment.Spec.Template.Spec.Containers[0].Ports[0].ContainerPort = proxy.Spec.Port
+	if proxy.Spec.MySQLDriver.Version != "" {
+		updateInitContainer(runtimeDeployment, proxy.Spec.MySQLDriver)
+	}
+	if proxy.Spec.Resources != nil {
+		runtimeDeployment.Spec.Template.Spec.Containers[0].Resources = *proxy.Spec.Resources
+	} else {
+		cpu, _ := resource.ParseQuantity("0.2")
+		memory, _ := resource.ParseQuantity("1.6Gi")
+		runtimeDeployment.Spec.Template.Spec.Containers[0].Resources = v1.ResourceRequirements{
+			Requests: v1.ResourceList{
+				"cpu":    cpu,
+				"memory": memory,
+			},
+		}
+	}
+	if proxy.Spec.LivenessProbe != nil {
+		runtimeDeployment.Spec.Template.Spec.Containers[0].LivenessProbe = proxy.Spec.LivenessProbe
+	} else {
+		runtimeDeployment.Spec.Template.Spec.Containers[0].LivenessProbe = &v1.Probe{
+			ProbeHandler: v1.ProbeHandler{
+				TCPSocket: &v1.TCPSocketAction{
+					Port: intstr.FromInt(int(proxy.Spec.Port)),
+				},
+			},
+
+			PeriodSeconds: 10,
+		}
+	}
+	if proxy.Spec.ReadinessProbe != nil {
+		runtimeDeployment.Spec.Template.Spec.Containers[0].ReadinessProbe = proxy.Spec.ReadinessProbe
+	} else {
+		runtimeDeployment.Spec.Template.Spec.Containers[0].ReadinessProbe = &v1.Probe{
+			ProbeHandler: v1.ProbeHandler{
+				TCPSocket: &v1.TCPSocketAction{
+					Port: intstr.FromInt(int(proxy.Spec.Port)),
+				},
+			},
+			PeriodSeconds: 10,
+		}
+	}
+	if proxy.Spec.StartupProbe != nil {
+		runtimeDeployment.Spec.Template.Spec.Containers[0].StartupProbe = proxy.Spec.StartupProbe
+	} else {
+		runtimeDeployment.Spec.Template.Spec.Containers[0].StartupProbe = &v1.Probe{
+			ProbeHandler: v1.ProbeHandler{
+				TCPSocket: &v1.TCPSocketAction{
+					Port: intstr.FromInt(int(proxy.Spec.Port)),
+				},
+			},
+			PeriodSeconds:    5,
+			FailureThreshold: 12,
+		}
+	}
+}
+
+func UpdateService(proxy *shardingspherev1alpha1.Proxy, runtimeService *v1.Service) {
+	runtimeService.Spec.Type = proxy.Spec.ServiceType.Type
+	runtimeService.Spec.Ports[0].Port = proxy.Spec.Port
+	runtimeService.Spec.Ports[0].TargetPort = fromInt32(proxy.Spec.Port)
+	if proxy.Spec.ServiceType.NodePort != 0 {
+		runtimeService.Spec.Ports[0].NodePort = proxy.Spec.ServiceType.NodePort
+	}
+}
+
+func fromInt32(val int32) intstr.IntOrString {
+	return intstr.IntOrString{Type: intstr.Int, IntVal: val}
 }
--- pkg/reconcile/status.go
+++ pkg/reconcile/status.go
@@ -1,17 +1,18 @@
 /*
- * Copyright (c) 2022.
+ *   Copyright © 2022，Beijing Sifei Software Technology Co., LTD.
+ *   All Rights Reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  */
 
 package reconcile
@@ -22,10 +23,10 @@ import (
 )
 
 func IsRunning(podList *v1.PodList) bool {
-	status := true
+	status := false
 	for _, pod := range podList.Items {
-		if pod.Status.Phase != v1.PodRunning {
-			status = false
+		if pod.Status.Phase == v1.PodRunning && pod.ObjectMeta.DeletionTimestamp == nil {
+			status = true
 		}
 	}
 
@@ -44,9 +45,9 @@ func CountingReadyPods(podList *v1.PodList) int32 {
 }
 
 func CountingPodMaxRestartTimes(podList *v1.PodList) int32 {
-	var podRestartCount int32 = math.MinInt32
+	var podRestartCount int32 = math.MaxInt32
 	for _, pod := range podList.Items {
-		if podRestartCount < pod.Status.ContainerStatuses[0].RestartCount {
+		if podRestartCount > pod.Status.ContainerStatuses[0].RestartCount {
 			podRestartCount = pod.Status.ContainerStatuses[0].RestartCount
 		}
 	}
