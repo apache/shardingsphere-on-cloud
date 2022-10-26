8a0a6194e5a7778f4bcb4e92a4fa65932512610f
senjaya <amarpanjis@gmail.com>
2022-10-26 19:35:16 +0700

    chore(reconcile): add unit test for ToYaml

--- shardingsphere-operator/pkg/reconcile/reconcile_test.go
+++ shardingsphere-operator/pkg/reconcile/reconcile_test.go
@@ -480,7 +480,97 @@ func Test_ConstructHPA(t *testing.T) {
 }
 
 func Test_ToYAML(t *testing.T) {
+	cases := []struct {
+		proxyCfg *v1alpha1.ShardingSphereProxyServerConfig
+		exp      string
+		message  string
+	}{
+		{
+			proxyCfg: &v1alpha1.ShardingSphereProxyServerConfig{},
+			exp: `mode:
+  type: ""
+  repository:
+    type: ""
+    props:
+      namespace: ""
+      server-lists: ""
+authority:
+  users: []
+  privilege: null
+`,
+			message: "Empty proxy server config should generate yaml with empty value",
+		},
+		{
+			proxyCfg: &v1alpha1.ShardingSphereProxyServerConfig{
+				Spec: v1alpha1.ProxyConfigSpec{
+					ClusterConfig: v1alpha1.ClusterConfig{
+						Type: "Cluster",
+						Repository: v1alpha1.RepositoryConfig{
+							Type: "Zookeeper",
+							Props: v1alpha1.ClusterProps{
+								Namespace:                    "cluster-sharding-mode",
+								ServerLists:                  "host1:2181,host2:2181",
+								RetryIntervalMilliseconds:    500,
+								MaxRetries:                   3,
+								TimeToLiveSeconds:            60,
+								OperationTimeoutMilliseconds: 500,
+							},
+						},
+					},
+					Authority: v1alpha1.Auth{
+						Users: []v1alpha1.User{
+							{
+								User:     "John",
+								Password: "123456",
+							},
+						},
+						Privilege: &v1alpha1.Privilege{
+							Type: "Admin",
+						},
+					},
+					Props: &v1alpha1.Props{
+						KernelExecutorSize:                16,
+						CheckTableMetadataEnabled:         false,
+						ProxyBackendQueryFetchSize:        -1,
+						CheckDuplicateTableEnabled:        false,
+						ProxyFrontendExecutorSize:         0,
+						ProxyBackendExecutorSuitable:      "OLAP",
+						ProxyBackendDriverType:            "JDBC",
+						ProxyFrontendDatabaseProtocolType: "",
+					},
+				},
+			},
+			exp: `mode:
+  type: Cluster
+  repository:
+    type: Zookeeper
+    props:
+      namespace: cluster-sharding-mode
+      server-lists: host1:2181,host2:2181
+      retryIntervalMilliseconds: 500
+      maxRetries: 3
+      timeToLiveSeconds: 60
+      operationTimeoutMilliseconds: 500
+authority:
+  users:
+  - user: John
+    password: "123456"
+  privilege:
+    type: Admin
+props:
+  kernel-executor-size: 16
+  proxy-backend-query-fetch-size: -1
+  proxy-backend-executor-suitable: OLAP
+  proxy-backend-driver-type: JDBC
+`,
+			message: "Should generate yaml with the same value as ShardingSphereProxyServerConfig.Spec",
+		},
+	}
 
+	for _, c := range cases {
+		act := toYaml(c.proxyCfg)
+		assert.Equal(t, c.exp, act, c.message)
+	}
 }
 
 func Test_UpdateDeployment(t *testing.T) {
