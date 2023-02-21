dee4c690e161becc4bc063052fd90e1c038de764
lilutong <llt.github@outlook.com>
2023-02-21 13:18:14 +0800

    feat:cli add logger

--- pitr/cli/go.mod
+++ pitr/cli/go.mod
@@ -3,7 +3,13 @@ module github.com/apache/shardingsphere-on-cloud/pitr/cli
 go 1.20
 
 require (
+	github.com/spf13/cobra v1.6.1
+	go.uber.org/zap v1.24.0
+)
+
+require (
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
-	github.com/spf13/cobra v1.6.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
 )
---
+++ pitr/cli/pkg/logging/log.go
@@ -0,0 +1,69 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements.  See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+ */
+
+package logging
+
+import "go.uber.org/zap"
+
+var l *ZapLogger
+
+func Log() ILog {
+	return l
+}
+
+func Init(logger *zap.Logger) ILog {
+	l = &ZapLogger{logger: logger}
+	return l
+}
+
+func Field(k FieldKey, v string) ILog {
+	m := map[FieldKey]string{k: v}
+	for k, v := range l.fields {
+		m[k] = v
+	}
+	return &ZapLogger{logger: l.logger, fields: m}
+}
+
+func Fields(m map[FieldKey]string) ILog {
+	if m == nil {
+		m = map[FieldKey]string{}
+	}
+	for k, v := range l.fields {
+		m[k] = v
+	}
+	return &ZapLogger{logger: l.logger, fields: m}
+}
+
+func Debug(s string) {
+	l.logger.Debug(s, fields(l)...)
+}
+
+func Info(s string) {
+	l.logger.Info(s, fields(l)...)
+}
+
+func Warn(s string) {
+	l.logger.Warn(s, fields(l)...)
+}
+
+func Error(s string) {
+	l.logger.Error(s, fields(l)...)
+}
+
+func Panic(s string) {
+	l.logger.Panic(s, fields(l)...)
+}
