2e115128cff4f052d7826b0e5e04519443653881
Xu-Wentao <cutenear1993@yahoo.com>
2023-03-22 12:53:00 +0800

    feat(pitr): implement show backup record func (#273)

--- pitr/cli/internal/cmd/show.go
+++ pitr/cli/internal/cmd/show.go
@@ -18,11 +18,19 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
-
-	"github.com/spf13/cobra"
-
+	"github.com/apache/shardingsphere-on-cloud/pitr/cli/internal/pkg"
+	"github.com/apache/shardingsphere-on-cloud/pitr/cli/internal/pkg/model"
+	"github.com/apache/shardingsphere-on-cloud/pitr/cli/internal/pkg/xerr"
 	"github.com/apache/shardingsphere-on-cloud/pitr/cli/pkg/logging"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var (
+	CSN      string
+	RecordID string
 )
 
 var Show = &cobra.Command{
@@ -59,28 +67,115 @@ var Show = &cobra.Command{
 		}
 		logging.Info(fmt.Sprintf("flags:agentPort:%d", agentPort))
 
-		csn, err := cmd.Flags().GetString(csn)
+		CSN, err = cmd.Flags().GetString(csn)
 		if err != nil {
 			logging.Error(err.Error())
 		}
-		logging.Info(fmt.Sprintf("flags:csn:%s", csn))
+		logging.Info(fmt.Sprintf("flags:csn:%s", CSN))
 
-		id, err := cmd.Flags().GetString(backupRecordID)
+		RecordID, err = cmd.Flags().GetString(backupRecordID)
 		if err != nil {
 			logging.Error(err.Error())
 		}
-		logging.Info(fmt.Sprintf("flags:id:%s", id))
+		logging.Info(fmt.Sprintf("flags:id:%s", RecordID))
 
-		logging.Info("Restore...")
+		logging.Info("Show backup history ...")
+
+		if err := show(); err != nil {
+			logging.Error(err.Error())
+		}
 	},
 }
 
-func init() {
-	var (
-		csn string
-		id  string
-	)
+func show() error {
+	root := fmt.Sprintf("%s/%s", os.Getenv("HOME"), ".gs_pitr")
+	logging.Info(fmt.Sprintf("Default backup path: %s\n", root))
 
-	Show.PersistentFlags().StringVarP(&csn, "csn", "", "", "commit sequence number")
-	Show.PersistentFlags().StringVarP(&id, "id", "", "", "backup record id")
+	ls, err := pkg.NewLocalStorage(root)
+	if err != nil {
+		return xerr.NewCliErr(fmt.Sprintf("connect to local storage failed, err:%s", err.Error()))
+	}
+	// show backup record by csn
+	if CSN != "" {
+		bak, err := ls.ReadByCSN(csn)
+		if bak == nil {
+			fmt.Println("Didn't find backup record by csn: ", CSN)
+			return nil
+		}
+		if err != nil {
+			return xerr.NewCliErr(fmt.Sprintf("read backup record failed, err:%s", err.Error()))
+		}
+		if err := formatRecord([]model.LsBackup{*bak}); err != nil {
+			return err
+		}
+		return nil
+	}
+	// show backup record by id
+	if RecordID != "" {
+		bak, err := ls.ReadByID(RecordID)
+		if bak == nil {
+			fmt.Println("Didn't find backup record by record id: ", RecordID)
+			return nil
+		}
+		if err != nil {
+			return xerr.NewCliErr(fmt.Sprintf("read backup record failed, err:%s", err.Error()))
+		}
+		if err := formatRecord([]model.LsBackup{*bak}); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	// show all backup record
+	backupList, err := ls.ReadAll()
+	if len(backupList) == 0 {
+		fmt.Println("Didn't find any backup record.")
+		return nil
+	}
+	if err != nil {
+		return xerr.NewCliErr(fmt.Sprintf("read backup record failed, err:%s", err.Error()))
+	}
+
+	if err := formatRecord(backupList); err != nil {
+		return err
+	}
+
+	return nil
+}
+func formatRecord(backups []model.LsBackup, mode ...string) error {
+	var m string
+
+	if len(mode) == 0 {
+		m = "json"
+	} else {
+		m = mode[0]
+	}
+	switch m {
+	case "json":
+		return formatRecordJson(backups)
+	case "table":
+		// TODO format record table
+		return nil
+	default:
+		return formatRecordJson(backups)
+	}
+}
+
+func formatRecordJson(backups []model.LsBackup) error {
+	var ds []string
+	for _, backup := range backups {
+		data, err := json.MarshalIndent(backup, "", "\t")
+		if err != nil {
+			logging.Error(err.Error())
+			return xerr.NewCliErr("format backup record failed, indent failed.")
+		}
+		ds = append(ds, string(data))
+	}
+	fmt.Println(ds)
+	return nil
+}
+
+func init() {
+	Show.PersistentFlags().StringVarP(&CSN, "csn", "", "", "commit sequence number")
+	Show.PersistentFlags().StringVarP(&RecordID, "id", "", "", "backup record id")
 }
