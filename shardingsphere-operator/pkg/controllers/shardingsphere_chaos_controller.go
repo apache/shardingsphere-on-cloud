648eee193654012e8ce9385ec19fd2b8fea3e30f
mlycore <maxwell92@126.com>
2023-03-27 15:44:20 +0800

    chore: add template for ShardingSphereChaos and StorageNode controllers
    Signed-off-by: mlycore <maxwell92@126.com>

---
+++ shardingsphere-operator/pkg/controllers/shardingsphere_chaos_controller.go
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import (
+	"context"
+
+	"github.com/apache/shardingsphere-on-cloud/shardingsphere-operator/api/v1alpha1"
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// ShardingSphereChaosReconciler is a controller for the ShardingSphereChaos
+type ShardingSphereChaosReconciler struct { //
+	client.Client
+	Scheme *runtime.Scheme
+	Log    logr.Logger
+}
+
+// SetupWithManager sets up the controller with the Manager
+func (r *ShardingSphereChaosReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&v1alpha1.ShardingSphereChaos{}).
+		Owns(&v1alpha1.ComputeNode{}).
+		Owns(&v1alpha1.StorageNode{}).
+		Complete(r)
+}
+
+// Reconcile handles main function of this controller
+func (r *ShardingSphereChaosReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	return ctrl.Result{}, nil
+}
