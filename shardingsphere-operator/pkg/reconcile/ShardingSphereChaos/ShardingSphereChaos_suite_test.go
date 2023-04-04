d1c959526d7ab5f9b97721fc121020ef3bdfc8ac
moonman <agoiyanzsa@163.com>
2023-04-04 11:17:01 +0800

    delete no-used tests

--- shardingsphere-operator/pkg/reconcile/ShardingSphereChaos/ShardingSphereChaos_suite_test.go
+++ shardingsphere-operator/pkg/reconcile/ShardingSphereChaos/ShardingSphereChaos_suite_test.go
@@ -16,118 +16,13 @@
  */
 package ShardingSphereChaos_test
 
-//
-//import (
-//	chaosV1alpha1 "github.com/chaos-mesh/chaos-mesh/api/v1alpha1"
-//	"k8s.io/client-go/rest"
-//	"path/filepath"
-//	ctrl "sigs.k8s.io/controller-runtime"
-//	"sigs.k8s.io/controller-runtime/pkg/client"
-//	"sigs.k8s.io/controller-runtime/pkg/envtest"
-//	logf "sigs.k8s.io/controller-runtime/pkg/log"
-//	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-//	"testing"
-//	"time"
-//)
-
-//func TestShardingSphereChaos(t *testing.T) {
-//	RegisterFailHandler(Fail)
-//	RunSpecs(t, "ShardingSphereChaos Suite")
-//}
-
-//todo: need fix schema
 import (
-	"context"
-	"github.com/apache/shardingsphere-on-cloud/shardingsphere-operator/api/v1alpha1"
-	"github.com/apache/shardingsphere-on-cloud/shardingsphere-operator/pkg/controllers"
-	"github.com/apache/shardingsphere-on-cloud/shardingsphere-operator/pkg/kubernetes/chaos"
-	chaosV1alpha1 "github.com/chaos-mesh/chaos-mesh/api/v1alpha1"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	batchV1 "k8s.io/api/batch/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/rest"
-	"path/filepath"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/envtest"
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"testing"
-	"time"
 )
 
 func TestShardingSphereChaos(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "ShardingSphereChaos Suite")
 }
-
-var (
-	cfg       *rest.Config
-	k8sClient client.Client
-	testEnv   *envtest.Environment
-	ctx       context.Context
-	cancel    context.CancelFunc
-	err       error
-)
-
-// +kubebuilder:scaffold:scheme
-var _ = BeforeSuite(func() {
-	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
-	ctx, cancel = context.WithCancel(context.TODO())
-	scheme := runtime.NewScheme()
-	err = clientgoscheme.AddToScheme(scheme)
-	Expect(err).NotTo(HaveOccurred())
-
-	err = batchV1.AddToScheme(scheme)
-	Expect(err).NotTo(HaveOccurred())
-
-	err = v1alpha1.AddToScheme(scheme)
-	Expect(err).NotTo(HaveOccurred())
-
-	err = chaosV1alpha1.AddToScheme(scheme)
-	Expect(err).NotTo(HaveOccurred())
-
-	By("bootstrapping test environment")
-	testEnv = &envtest.Environment{
-		CRDDirectoryPaths:     []string{filepath.Join("..", "..", "..", "config", "crd", "bases")},
-		ErrorIfCRDPathMissing: true,
-		CRDInstallOptions: envtest.CRDInstallOptions{
-			MaxTime: 60 * time.Second,
-		},
-	}
-	cfg, err = testEnv.Start()
-	Expect(err).NotTo(HaveOccurred())
-	Expect(cfg).NotTo(BeNil())
-
-	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme})
-	Expect(err).NotTo(HaveOccurred())
-	Expect(k8sClient).NotTo(BeNil())
-
-	k8sManager, err := ctrl.NewManager(cfg, ctrl.Options{
-		Scheme: scheme,
-	})
-	Expect(err).ToNot(HaveOccurred())
-
-	err = (&controllers.ShardingSphereChaosReconciler{
-		Client: k8sClient,
-		Scheme: scheme,
-		Chaos:  chaos.NewChaos(k8sClient),
-		Log:    logf.Log,
-	}).SetupWithManager(k8sManager)
-	Expect(err).ToNot(HaveOccurred())
-
-	go func() {
-		defer GinkgoRecover()
-		err = k8sManager.Start(ctx)
-		Expect(err).ToNot(HaveOccurred(), "failed to run manager")
-	}()
-})
-
-var _ = AfterSuite(func() {
-	cancel()
-	By("tearing down the test environment")
-	err = testEnv.Stop()
-	Expect(err).NotTo(HaveOccurred())
-})
