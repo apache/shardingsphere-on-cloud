a282e4454fd93e2e7d25a6c1791e0de3a47ef07e
moomman <85389467+moomman@users.noreply.github.com>
2023-04-07 17:21:13 +0800

    Rebase main
    ---------
    
    Signed-off-by: mlycore <maxwell92@126.com>
    Co-authored-by: Xu-Wentao <cutenear1993@yahoo.com>
    Co-authored-by: liyao <maxwell92@126.com>
    Co-authored-by: Zhengqiang Duan <duanzhengqiang@apache.org>
    Co-authored-by: moonman <agoiyanzsa@163.com>

--- shardingsphere-operator/api/v1alpha1/shardingsphere_chaos_types.go
+++ shardingsphere-operator/api/v1alpha1/shardingsphere_chaos_types.go
@@ -17,7 +17,9 @@
 
 package v1alpha1
 
-import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
 
 // +kubebuilder:object:root=true
 // ShardingSphereChaosList contains a list of ShardingSphereChaos
@@ -34,14 +36,192 @@ type ShardingSphereChaosList struct {
 type ShardingSphereChaos struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
-
-	Spec ShardingSphereChaosSpec `json:"spec,omitempty"`
-	// +optional
-	Status ShardingSphereChaosStatus `json:"status,omitempty"`
+	Spec              ShardingSphereChaosSpec   `json:"spec,omitempty"`
+	Status            ShardingSphereChaosStatus `json:"status,omitempty"`
 }
 
 // ShardingSphereChaosSpec defines the desired state of ShardingSphereChaos
-type ShardingSphereChaosSpec struct{}
+type ShardingSphereChaosSpec struct {
+	//todo
+	//InjectJob batchV1Beta1.JobTemplateSpec `json:"InjectJob,omitempty"`
+	ChaosKind  ChaosKind `json:"chaosKind,omitempty"`
+	EmbedChaos `json:",inline"`
+	//todo
+	//Verify batchV1Beta1.JobTemplateSpec `json:"Verify,omitempty"`
+}
+
+// ChaosKind Specifies the type of chaos to create
+type ChaosKind string
+
+const (
+	NetworkChaosKind ChaosKind = "networkChaos"
+	PodChaosKind     ChaosKind = "podChaos"
+)
+
+type EmbedChaos struct {
+	// +optional
+	NetworkChaos *NetworkChaosSpec `json:"networkChaos,omitempty"`
+	// +optional
+	PodChaos *PodChaosSpec `json:"podChaos,omitempty"`
+}
+
+// ChaosCondition Show Chaos Progress
+type ChaosCondition string
+
+const (
+	Creating     ChaosCondition = "Creating"
+	AllRecovered ChaosCondition = "AllRecovered"
+	Paused       ChaosCondition = "Paused"
+	AllInjected  ChaosCondition = "AllInjected"
+	NoTarget     ChaosCondition = "NoTarget"
+	UnKnown      ChaosCondition = "UnKnown"
+)
+
+// Jobschedule Show current job progress
+type Jobschedule string
+
+const (
+	JobCreating Jobschedule = "JobCreating"
+	JobFailed   Jobschedule = "JobFailed"
+	JobFinish   Jobschedule = "JobFinish"
+)
 
 // ShardingSphereChaosStatus defines the actual state of ShardingSphereChaos
-type ShardingSphereChaosStatus struct{}
+type ShardingSphereChaosStatus struct {
+	ChaosCondition ChaosCondition `json:"chaosCondition"`
+	//todo
+	//InjectStatus   Jobschedule         `json:"InjectStatus"`
+	//todo
+	//VerifyStatus   Jobschedule         `json:"VerifyStatus"`
+}
+
+// PodChaosAction Specify the action type of pod Chaos
+type PodChaosAction string
+
+var (
+	PodFailureAction    PodChaosAction = "podFailure"
+	ContainerKillAction PodChaosAction = "containerKill"
+)
+
+// PodChaosSpec Fields that need to be configured for pod type chaos
+type PodChaosSpec struct {
+	PodSelector `json:"selector,omitempty"`
+	Action      PodChaosAction `json:"action"`
+	//+optional
+	PodActionParam PodActionParam `json:"params,omitempty"`
+}
+
+// PodActionParam Optional parameters for pod type configuration
+type PodActionParam struct {
+	//+optional
+	PodFailure PodFailureActionParams `json:"podFailure,omitempty"`
+	//+optional
+	ContainerKill ContainerKillActionParams `json:"containerKill,omitempty"`
+}
+
+type PodFailureActionParams struct {
+	// +optional
+	Duration string `json:"duration,omitempty"`
+}
+
+type ContainerKillActionParams struct {
+	// +optional
+	ContainerNames []string `json:"containerNames,omitempty"`
+}
+
+// NetworkChaosSpec Fields that need to be configured for network type chaos
+type NetworkChaosSpec struct {
+	Source PodSelector `json:",inline"`
+	// +optional
+	Duration *string `json:"duration,omitempty"`
+	//+optional
+	Direction Direction `json:"direction,omitempty"`
+	// +optional
+	Target *PodSelector       `json:"target,omitempty"`
+	Action NetworkChaosAction `json:"action"`
+	// +optional
+	Network *NetworkParams `json:"params,omitempty"`
+}
+
+// NetworkParams Optional parameters for network type configuration
+type NetworkParams struct {
+	// +optional
+	Delay *DelayActionParams `json:"delay,omitempty"`
+	// +optional
+	Loss *LossActionParams `json:"loss,omitempty"`
+	// +optional
+	Duplicate *DuplicateActionParams `json:"duplicate,omitempty"`
+	// +optional
+	Corrupt *CorruptActionParams `json:"corrupt,omitempty"`
+}
+
+type DelayActionParams struct {
+	// +optional
+	Latency string `json:"latency,omitempty"`
+	// +optional
+	Correlation string `json:"correlation,omitempty"`
+	// +optional
+	Jitter string `json:"jitter,omitempty"`
+}
+
+type LossActionParams struct {
+	// +optional
+	Loss string `json:"loss,omitempty"`
+	// +optional
+	Correlation string `json:"correlation,omitempty"`
+}
+
+type DuplicateActionParams struct {
+	// +optional
+	Duplicate string `json:"duplicate,omitempty"`
+	// +optional
+	Correlation string `json:"correlation,omitempty"`
+}
+
+type CorruptActionParams struct {
+	// +optional
+	Corrupt string `json:"corrupt,omitempty"`
+	// +optional
+	Correlation string `json:"correlation,omitempty"`
+}
+
+// NetworkChaosAction Specify the action type of network Chaos
+type NetworkChaosAction string
+
+const (
+	DelayAction     NetworkChaosAction = "delay"
+	LossAction      NetworkChaosAction = "loss"
+	DuplicateAction NetworkChaosAction = "duplicate"
+	CorruptAction   NetworkChaosAction = "corrupt"
+	PartitionAction NetworkChaosAction = "partition"
+)
+
+// Direction Specifies the direction of action of network chaos
+type Direction string
+
+const (
+	To   Direction = "to"
+	From Direction = "from"
+	Both Direction = "both"
+)
+
+// PodSelector Used to select the target of the specified chaos
+type PodSelector struct {
+	// +optional
+	Namespaces []string `json:"namespaces,omitempty"`
+	// +optional
+	LabelSelectors map[string]string `json:"labelSelectors,omitempty"`
+	// +optional
+	AnnotationSelectors map[string]string `json:"annotationSelectors,omitempty"`
+	// +optional
+	Nodes []string `json:"nodes,omitempty"`
+	// +optional
+	Pods map[string][]string `json:"pods,omitempty"`
+	// +optional
+	NodeSelectors       map[string]string                 `json:"nodeSelectors,omitempty"`
+	ExpressionSelectors []metav1.LabelSelectorRequirement `json:"expressionSelectors,omitempty"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ShardingSphereChaos{}, &ShardingSphereChaosList{})
+}
