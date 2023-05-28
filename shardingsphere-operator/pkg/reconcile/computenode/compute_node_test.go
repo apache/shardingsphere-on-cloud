9ad041b6599bf6f05092e89a71eb97807f160501
mlycore <maxwell92@126.com>
2023-05-28 17:52:49 +0800

    chore: add unit test
    Signed-off-by: mlycore <maxwell92@126.com>

--- shardingsphere-operator/pkg/reconcile/computenode/compute_node_test.go
+++ shardingsphere-operator/pkg/reconcile/computenode/compute_node_test.go
@@ -16,3 +16,101 @@
  */
 
 package computenode
+
+import (
+	"testing"
+
+	"github.com/apache/shardingsphere-on-cloud/shardingsphere-operator/api/v1alpha1"
+	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+)
+
+func Test_getPreferedConditionFromPodConditions(t *testing.T) {
+	cases := []struct {
+		pcs     []corev1.PodCondition
+		expect  []v1alpha1.ComputeNodeCondition
+		message string
+	}{
+		{
+			pcs: []corev1.PodCondition{
+				corev1.PodCondition{
+					Type:   corev1.PodScheduled,
+					Status: corev1.ConditionTrue,
+				},
+			},
+			expect: []v1alpha1.ComputeNodeCondition{
+				{
+					Type: v1alpha1.ComputeNodeConditionDeployed,
+				},
+			},
+			message: "scheduled pod condition should get deployed condition",
+		},
+	}
+	for _, c := range cases {
+		act := getPreferedConditionFromPodConditions(c.pcs)
+		assert.Equal(t, len(c.expect), len(act), c.message)
+		for i := range act {
+			assert.Equal(t, c.expect[i].Type, act[i].Type, c.message)
+		}
+	}
+}
+
+func Test_getPreferedConditionFromPod(t *testing.T) {
+	cases := []struct {
+		pod     corev1.Pod
+		expect  []v1alpha1.ComputeNodeCondition
+		message string
+	}{
+		{
+			pod: corev1.Pod{
+				Status: corev1.PodStatus{
+					Phase: corev1.PodPending,
+				},
+			},
+			expect: []v1alpha1.ComputeNodeCondition{
+				{
+					Type:   v1alpha1.ComputeNodeConditionPending,
+					Status: v1alpha1.ConditionStatusTrue,
+				},
+			},
+			message: "pending pod should get pending condition",
+		},
+	}
+
+	for _, c := range cases {
+		act := getPreferedConditionFromPod(c.pod)
+		assert.Equal(t, len(c.expect), len(act), c.message)
+		for i := range act {
+			assert.Equal(t, c.expect[i].Type, act[i].Type, c.message)
+		}
+	}
+}
+
+func Test_GetConditionFromPods(t *testing.T) {
+	cases := []struct {
+		podlist *corev1.PodList
+		expect  []v1alpha1.ComputeNodeCondition
+		message string
+	}{
+		{
+			podlist: &corev1.PodList{Items: []corev1.Pod{}},
+			expect: []v1alpha1.ComputeNodeCondition{
+				{
+					Type:   v1alpha1.ComputeNodeConditionUnknown,
+					Status: v1alpha1.ConditionStatusTrue,
+				},
+			},
+			message: "empty podlist should get unknown condition",
+		},
+	}
+
+	for _, c := range cases {
+		act := GetConditionFromPods(c.podlist)
+		assert.Equal(t, len(c.expect), len(act), c.message)
+		for i := range act {
+			assert.Equal(t, c.expect[i].Type, act[i].Type, c.message)
+			assert.Equal(t, c.expect[i].Status, act[i].Status, c.message)
+		}
+	}
+
+}
