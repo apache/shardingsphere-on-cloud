1b2ae3f57bcbaf5e371f32fc72ed16557cb2cf13
liyao <maxwell92@126.com>
2023-10-25 19:04:50 +0800

    chore: update proxy version in operator (#449)
    * chore: update example proxy version
    
    Signed-off-by: mlycore <maxwell92@126.com>
    
    * chore: update supported version
    
    Signed-off-by: mlycore <maxwell92@126.com>
    Signed-off-by: mlycore <miaoliyao@apache.org>
    
    ---------
    
    Signed-off-by: mlycore <maxwell92@126.com>
    Signed-off-by: mlycore <miaoliyao@apache.org>

--- shardingsphere-operator/pkg/kubernetes/helpers.go
+++ shardingsphere-operator/pkg/kubernetes/helpers.go
@@ -24,7 +24,7 @@ import (
 )
 
 var (
-	supportedShardingSphereVersion = []string{"5.3.0", "5.3.1", "5.3.2", "5.4.0"}
+	supportedShardingSphereVersion = []string{"5.3.0", "5.3.1", "5.3.2", "5.4.0", "5.4.1"}
 )
 
 func patchHeadingV(version string) string {
