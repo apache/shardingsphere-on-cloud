4a302d1a65dc086e7686c20781b14ea2688e4776
mlycore <maxwell92@126.com>
2023-12-28 18:07:02 +0800

    fix: fix no metadata database found
    Signed-off-by: mlycore <maxwell92@126.com>

--- pitr/cli/internal/cmd/restore.go
+++ pitr/cli/internal/cmd/restore.go
@@ -151,6 +151,10 @@ func checkDatabaseExist(proxy pkg.IShardingSphereProxy, bak *model.LsBackup) err
 		return xerr.NewCliErr(fmt.Sprintf("get cluster metadata failed. err: %s", err))
 	}
 
+	if clusterNow.MetaData.Databases == nil {
+		return xerr.NewCliErr(fmt.Sprintf("no cluster metadata database found. err: %s", err))
+	}
+
 	for k := range clusterNow.MetaData.Databases {
 		databaseNamesExist = append(databaseNamesExist, k)
 	}
